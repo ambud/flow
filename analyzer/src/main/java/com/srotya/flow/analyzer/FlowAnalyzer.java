d47494e770e6fe6bf68f683b28dfb17027d2008c
ambud <asharma525xi@gmail.com>
2016-04-13 18:42:01 -0700

    Simple netflow collector and analyzer projects

---
+++ analyzer/src/main/java/com/srotya/flow/analyzer/FlowAnalyzer.java
@@ -0,0 +1,21 @@
+package com.srotya.flow.analyzer;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Environment;
+
+/**
+ * @author ambudsharma
+ */
+public class FlowAnalyzer extends Application<FlowConfiguration> {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+	@Override
+	public void run(FlowConfiguration arg0, Environment arg1) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
